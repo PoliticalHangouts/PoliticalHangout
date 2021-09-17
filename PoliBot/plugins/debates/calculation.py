4408e010c4fbc5325074a30b496152cf2e6c9034
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-17 01:58:58 -0500

    Update calculation.py
    set k factor to 18.5

--- PoliBot/plugins/debates/calculation.py
+++ PoliBot/plugins/debates/calculation.py
@@ -4,5 +4,5 @@ def calculate_points(
   i: int,
   j: int
 ) -> typing.Tuple(int, int):
-  score_factor = int(i + j + 200 / i * 32)
+  score_factor = int(i + j + 200-18.5 / i * 18.5)
   return (i + score_facor, j - score_factor)
