666e9fe360a8b91608f3f5e13698550156c1eb6e
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-16 20:27:46 -0500

    Create main.py

---
+++ PoliBot/main.py
@@ -0,0 +1,44 @@
+import discord
+import os
+
+from discord.ext import commands
+from discord_slash import SlashCommand
+
+bot = commands.Bot(
+  command_prefix="$",
+  intents=discord.Intents.all(),
+  self_bot=False,
+  description="A discord bot made to handle the guild system for the Political Hangout discord server.",
+  help_command=None,
+  case_insensitive=True,
+  owner_ids=[
+    707643377621008447,
+    479576984205131787,
+    292484057139380225,
+    750971217489428480,
+    581850206853791755,
+    867145519255781437,
+    592510722647261204,
+    767201711114420247,
+    339436064039239680,
+  ],
+  strip_after_prefix=True,
+)
+
+slash = SlashCommand(bot)
+
+def main() -> None:
+  cogs = [
+    'plugins.core.error.error_handling',
+    'plugins.core.startup.login',
+    'plugins.guilds.quiz',
+    'plugins.guilds.guild_permissions',
+    'plugins.debates.debate',
+  ]
+  for cog in cogs:
+    bot.load_extension(cog)
+    
+  bot.run(os.getenv("TOKEN"))
+
+if __name__ == "__main__":
+  main()
