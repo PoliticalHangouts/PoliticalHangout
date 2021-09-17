d4d5d65c583a719e12e90bd61b9e1e0a873eb79e
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-17 02:25:29 -0500

    Update calculation.py

--- PoliBot/plugins/debates/calculation.py
+++ PoliBot/plugins/debates/calculation.py
@@ -4,5 +4,7 @@ def calculate_points(
   i: int,
   j: int
 ) -> typing.Tuple(int, int):
-  score_factor = 5-int(i + j / 181.5)
+  p = i+j
+  _ = 200-18.5/p
+  score_factor = 5-int(i + j / _)
   return (i + int(18.5*score_factor), j - int(18.5*score_factor))
